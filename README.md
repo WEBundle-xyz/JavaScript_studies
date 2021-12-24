c6a167d02b0699b9538af8a7e3f08c3fd3075735
WEBundleXYZ <stefg@webundlexyzsmbp.mynetworksettings.com>
2021-12-24 12:07:45 -0500

    Rest and Spread operator

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #### JS Advanced function
 
-[1 - Project setup ](../../tree//)
+[1 - Project setup ](../../tree/ee57cfe84d9b418f9844cdfee85f3380365e41a0/)
 
 [2 - Empty ](../../tree//)
 
--- functions.js
+++ functions.js
@@ -1,6 +1,35 @@
-function greetUser(userName) {
-	console.log('Hi ' + userName + '!');
+function greetUser(greetingPrefix, userName = 'user') {
+	console.log(greetingPrefix + ' ' + userName + '!');
 }
 
-greetUser('Stefan');
-greetUser();
+// greetUser('HI', 'Stefan');
+// greetUser('Hello');
+
+// function sumUp(numbers) {
+//   let result = 0;
+
+//   for (const number of numbers) {
+//     result += number;
+//   }
+
+//   return result;
+
+// }
+
+// console.log(sumUp([3, 54, 54, 12, 1, 4]);
+
+
+function sumUp(...numbers) {
+  let result = 0;
+
+  for (const number of numbers) {
+    result += number;
+  }
+
+  return result;
+
+}
+
+const inputNumbers = [3, 54, 54, 12, 1, 4, 3, 6, 78];
+
+console.log(sumUp(...inputNumbers));
